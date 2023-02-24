46e8adfc599d52266b41c14f1a50d92a5cd2ce55
unknown <domilegotovtaite@gmail.com>
2023-02-24 17:12:10 +0200

    finished project

--- ceaser.py
+++ ceaser.py
@@ -27,10 +27,41 @@
 # > QIIX QI FC XLI VSWI FYWLIW XSRMKLX.
 # MEET ME BY THE ROSE BUSHES TONIGHT.
 
-alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', '<', '=', '>', '?','!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', 
-            ',', '‑', '.', '/', '@']
+import string
+import re
+alphabet = string.ascii_uppercase
 
+
+def encrypt_or_decrypt():
+    answer = input("Do You want to encrypt (type 'e') or decrypt (type 'd')? \n").lower()
+    while answer not in ['e','d']:
+        print("You have typed wrong symbol, try again.")
+        answer = input("Do You want to encrypt (type 'e') or decrypt (type 'd')?\n")
+    return answer
+
+
+def get_validate_key():
+    key_ = int(input("Please enter the key (0 to 26) to use.\n"))
+    while not isinstance(key_, int) or not (0 <= key_ <= 26):
+        print("You have entered an invalid key. Please enter an integer between 0 and 26.")  
+        key_ = int(input("Please enter the key (0 to 26) to use.\n"))
+    return key_
+
+
+def get_validate_text():
+    text = (input("Enter the message to encrypt.\n")).upper()        
+    while not re.match(r'^[\w\s:<>?!"#$%&\'()*+,‑./@]+$', text):
+        print("Text must contain latin letters, symbols or numbers.")       
+        text = (input("Enter the message to encrypt.\n")).upper()
+    return text
+
+
+def end_dec_again():
+    again_answer = input("Do You want to encrypt / decrypt again? (y/n)")  
+    while again_answer not in ['y', 'n']:
+        print("You have typed wrong symbol, try again (y/n).")
+        again_answer = input("Do You want to encrypt / decrypt again? (y/n)")  
+    return again_answer
 
 #going through text, checking for spaces and adding spaces to text if found
 # going through alphabet checking if letters from text is the same as letter
@@ -56,6 +87,7 @@ def encrypt(text, key_):
         # from alphabet if yes then counting new index by adding key to index of letter of alphabet.
         # Modulo is clarify that newly found index is in the range of alphabet.
 
+
 def decrypt(text, key_):
     decrypted_text = ""
     for i in range(len(text)):       
@@ -75,36 +107,25 @@ def decrypt(text, key_):
 # if letters in message only contain characters in alphabet
 # receives inputs if user want to continue to use a programme or not
 
+
 def run_cipher():
     result = ''
     while True:
-        answer = input("Do You want to encrypt (type 'e') or decrypt (type 'd')? \n").lower()
-        while answer not in ['e','d']:
-            print("You have typed wrong symbol, try again.")
-            answer = input("Do You want to encrypt (type 'e') or decrypt (type 'd')?\n")
-        key_ = int(input("Please enter the key (0 to 26) to use.\n"))
-        while not isinstance(key_, int) or not (0 <= key_ <= 26):
-            print("You have entered an invalid key. Please enter an integer between 0 and 26.")  
-            key_ = int(input("Please enter the key (0 to 26) to use.\n"))
-        text = (input("Enter the message to encrypt.\n")).upper()        
-        while not all(symbol in alphabet for symbol in text):
-            print("Text must contain latin letters, symbols or numbers.")       
-            text = (input("Enter the message to encrypt.\n")).upper()
+        answer = encrypt_or_decrypt()
+        key_ = get_validate_key()
+        text = get_validate_text()        
         if answer == 'e':
-            result = encrypt(text, key_)
-            print(result)          
+            result = encrypt(text, key_)          
         elif answer =='d':
             result = decrypt(text, key_)
-            print(result)
-        again_answer = input("Do You want to encrypt / decrypt again? (y/n)")  
-        while again_answer not in ['y', 'n']:
-            print("You have typed wrong symbol, try again (y/n).")
-            again_answer = input("Do You want to encrypt / decrypt again? (y/n)")  
+        print(result)
+        again_answer = end_dec_again()    
         if again_answer =='n':
             break
 
-
 run_cipher()
+
+
 
 
     
